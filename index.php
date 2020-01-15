2c2502d5f08adf01c56b1695c9cb3f0cf895f577
Maureen Kariuki <mkariuki2@mail.greenriver.edu>
2020-01-15 14:39:06 -0500

    Loaded fat free

--- index.php
+++ index.php
@@ -6,8 +6,23 @@
  * /328/chicken/index.php
  */
 
+//Controller fr this project
  //Turn on error reporting -- this is critical
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+//Require the autoload file
+require('vendor/autoload.php');
+
+//Instantiate fat free
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo "Hello";
+});
+
+//Run F3
+$f3->run();
 ?>
 
